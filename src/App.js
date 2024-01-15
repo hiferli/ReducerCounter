3f5115ab78a9973659e88faa963ec96662206fbc
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-01-15 15:22:22 +0530

    Creating Hooks and Integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useAppSelector } from './Store/Hooks/Index'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const count = useAppSelector(state => state.counter);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>Counter: {count} </h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
